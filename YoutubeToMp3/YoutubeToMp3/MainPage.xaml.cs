6e508d5704b5ddae8be147bafb0316fc59d8645d
Justin <justinengle01@gmail.com>
2018-04-25 21:20:24 +0200

    Progress ring and gets video info

--- YoutubeToMp3/YoutubeToMp3/MainPage.xaml.cs
+++ YoutubeToMp3/YoutubeToMp3/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using VideoLibrary;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.ViewManagement;
@@ -13,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using YoutubeExtractor;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -37,21 +39,57 @@ namespace YoutubeToMp3
     {
 
       var folderPicker = new Windows.Storage.Pickers.FolderPicker();
-      folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.MusicLibrary;
+      folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
       folderPicker.FileTypeFilter.Add("*");
 
       Windows.Storage.StorageFolder folder = await folderPicker.PickSingleFolderAsync();
       if (folder != null)
       {
+        // Application now has read/write access to all contents in the picked folder
+        // (including other sub-folder contents)
         Windows.Storage.AccessCache.StorageApplicationPermissions.
         FutureAccessList.AddOrReplace("PickedFolderToken", folder);
-        txtPath.Text =  folder.Path;
+        txtPath.Text = folder.Path;
         savePath = folder.Path;
       }
       else
       {
         txtPath.Text = choosePath;
       }
+
+    }
+
+    private async void btnDetails_Click(object sender, RoutedEventArgs e)
+    {
+      DataContext = null;
+     var youTube = YouTube.Default; // starting point for YouTube actions
+      ToggleProgressRing(true);     
+      var video = await youTube.GetVideoAsync(txtURL.Text); // gets a Video object with info about the video
+      DataContext = video;
+      ToggleProgressRing(false);
+    }
+
+    private void ToggleProgressRing(bool active)
+    {
+      string[] randomText =
+      {
+        "Wait wait wait...I'm busy",
+        "I'm still alive..just thinking",
+        "Either I'm slow or your internet sucks",
+        "Waiting for Youtube...."
+      };
+      if (active)
+      {
+        var rnd = new Random();
+        waitingTxt.Text = randomText[rnd.Next(0, randomText.Length - 1)];
+      }
+      progress.IsActive = active;
+      progress.Visibility = active ? Visibility.Visible : Visibility.Collapsed;
+      waitingTxt.Visibility = active ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    private void btnDownload_Click(object sender, RoutedEventArgs e)
+    {
 
     }
   }
